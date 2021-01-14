0fd04695b280f612c8a62bc52f1ba656579b8074
Charles Jackson <cbj170230@utdallas.edu>
2021-01-14 17:43:52 -0600

    Use sexp comments

--- testing.lisp
+++ testing.lisp
@@ -1,29 +1,29 @@
 (ql:quickload 'portal)
-;; (assert (equal (header->alist
-;;                 (str:join +crlf+ '("GET /chat?name=value HTTP/1.1"
-;;                                    "Host: example.com:8000"
-;;                                    "Upgrade: websocket"
-;;                                    "Connection: Upgrade"
-;;                                    "Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ=="
-;;                                    "Sec-WebSocket-Version: 13" "")))
-;;                '((:METHOD . "GET") (:SCRIPT . "/chat") (:QUERY-STRING . "name=value")(:VERSION . "HTTP/1.1")
-;;                  (:HOST . "example.com:8000") (:UPGRADE . "websocket")
-;;                  (:CONNECTION . "Upgrade") (:SEC-WEBSOCKET-KEY . "dGhlIHNhbXBsZSBub25jZQ==")
-;;                  (:SEC-WEBSOCKET-VERSION . "13"))))
+#++ (assert (equal (header->alist
+                (str:join +crlf+ '("GET /chat?name=value HTTP/1.1"
+                                   "Host: example.com:8000"
+                                   "Upgrade: websocket"
+                                   "Connection: Upgrade"
+                                   "Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ=="
+                                   "Sec-WebSocket-Version: 13" "")))
+               '((:METHOD . "GET") (:SCRIPT . "/chat") (:QUERY-STRING . "name=value")(:VERSION . "HTTP/1.1")
+                 (:HOST . "example.com:8000") (:UPGRADE . "websocket")
+                 (:CONNECTION . "Upgrade") (:SEC-WEBSOCKET-KEY . "dGhlIHNhbXBsZSBub25jZQ==")
+                 (:SEC-WEBSOCKET-VERSION . "13"))))
 
-;; (assert (string= (alist->header
-;;                   '((:version . "HTTP/1.1")
-;;                     (:code . "101")
-;;                     (:code-meaning . "Switching Protocols")
-;;                     (:upgrade . "websocket")
-;;                     (:connection . "Upgrade")
-;;                     (:sec-websocket-accept . "s3pPLMBiTxaQ9kYGzzhZRbK+xOo=")))
-;;                  (concatenate 'string (str:join +crlf+
-;;                                                 '("HTTP/1.1 101 Switching Protocols"
-;;                                                   "UPGRADE: websocket"
-;;                                                   "CONNECTION: Upgrade"
-;;                                                   "SEC-WEBSOCKET-ACCEPT: s3pPLMBiTxaQ9kYGzzhZRbK+xOo="))
-;;                               +crlf+ +crlf+)))
+#++ (assert (string= (alist->header
+                  '((:version . "HTTP/1.1")
+                    (:code . "101")
+                    (:code-meaning . "Switching Protocols")
+                    (:upgrade . "websocket")
+                    (:connection . "Upgrade")
+                    (:sec-websocket-accept . "s3pPLMBiTxaQ9kYGzzhZRbK+xOo=")))
+                 (concatenate 'string (str:join +crlf+
+                                                '("HTTP/1.1 101 Switching Protocols"
+                                                  "UPGRADE: websocket"
+                                                  "CONNECTION: Upgrade"
+                                                  "SEC-WEBSOCKET-ACCEPT: s3pPLMBiTxaQ9kYGzzhZRbK+xOo="))
+                              +crlf+ +crlf+)))
 
 (use-package 'arrows)
 (defparameter global-socket nil)
